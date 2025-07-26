c23dfd4abd0940394321091ec2ce59330e56079c
Pauloxhbh <109999895+Pauloxhbh@users.noreply.github.com>
2025-07-26 06:51:52 -0300

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,11 +1,13 @@
 class Header extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({ mode: 'open' });
+  }
 
-    const shadow = this.attachShadow({ mode: 'open' });
-    const pagesOrigin = window.location.origin == "https://pauloxhbh.github.io" ? window.location.origin + "/pokemon-eclipse-wiki" : window.location.origin
+  connectedCallback() {
+    const pagesOrigin = window.location.origin === "https://pauloxhbh.github.io" ? window.location.origin + "/pokemon-eclipse-wiki" : window.location.origin;
 
-    shadow.innerHTML = `
+    this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="${pagesOrigin}/css/style.css" />
       <header id="main-header">
         <button id="menu-toggle" aria-label="Abrir menu de navegação">&#9776;</button>
@@ -13,12 +15,11 @@ class Header extends HTMLElement {
       </header>
     `;
 
-    const menuToggle = shadow.getElementById("menu-toggle");
+    const menuToggle = this.shadowRoot.querySelector("#menu-toggle");
 
-    // Emite evento customizado para o document
     if (menuToggle) {
       menuToggle.addEventListener("click", () => {
-        this.dispatchEvent(new CustomEvent("toggle-menu", {
+        document.dispatchEvent(new CustomEvent("toggle-menu", {
           bubbles: true,
           composed: true
         }));
